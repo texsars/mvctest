48ef1a1cf7b300572b4eab7dd0d0e87c1c153208
fenghp <hyperspy@126.com>
2015-09-02 11:15:09 +0800

    Add index.jsp=>index.action map.

--- src/main/java/com/fhp/mvctest/NavigationController.java
+++ src/main/java/com/fhp/mvctest/NavigationController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class NavigationController {
 	
+	@RequestMapping(value="/user/index.action")
+	public String UserIndexNavigate(@PathVariable("page") String page) {
+		return "/user/index";
+	}
 	@RequestMapping(value="/user/{page}_nav.action")
 	public String UserNavigate(@PathVariable("page") String page) {
 		return "/user/" + page;
