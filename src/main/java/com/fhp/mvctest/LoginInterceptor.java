6b4944adff430830d75559fe4822924392b21c18
fenghp <hyperspy@126.com>
2015-09-02 13:31:49 +0800

    Modify the log level of the package com.fhp.mvctest

--- src/main/java/com/fhp/mvctest/LoginInterceptor.java
+++ src/main/java/com/fhp/mvctest/LoginInterceptor.java
@@ -7,10 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LoginInterceptor extends HandlerInterceptorAdapter {
+	private static Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+	
 	public boolean preHandle(HttpServletRequest request, 
 			HttpServletResponse response,
 			Object handler) throws IOException {
@@ -20,9 +24,11 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		AccessRequired annotation_method = method.getAnnotation(AccessRequired.class);
 		AccessRequired annotation_class = (AccessRequired) clazz.getAnnotation(AccessRequired.class);
 		if(null != annotation_method || null != annotation_class) {
+			logger.debug("已经过拦截器LoginInterceptor。");
 			HttpSession session = request.getSession();
 			if(null == session.getAttribute("user")) {
 				response.sendRedirect(request.getContextPath() + "/login.action");
+				logger.debug("用户未登录，已拦截LoginInterceptor。");
 				return false;
 			}
 		}
