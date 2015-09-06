c520aecbd306884c6fab5e0191266972c189d0d2
fenghp <hyperspy@126.com>
2015-09-06 13:39:20 +0800

    Add Entity.

---
+++ src/main/java/com/fhp/mvctest/entity/User.java
@@ -0,0 +1,33 @@
+package com.fhp.mvctest.entity;
+
+public class User {
+	private int id;
+	private String username;
+	private String nickname;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
